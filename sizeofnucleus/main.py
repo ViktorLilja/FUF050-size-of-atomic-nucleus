5313ba6df3936646b229c1d5359b3667e349b691
T00KER <gottfridolsson@hotmail.com>
2022-03-29 09:58:16 +0200

    Numerical optimization and progress

--- sizeofnucleus/main.py
+++ sizeofnucleus/main.py
@@ -1,6 +1,15 @@
+from sysconfig import get_path
 import numpy as np
+from scipy.optimize import minimize
 import physicsconstants as pc
 from crossections import *
+import pandas as pd
+import os
+
+# GLOBAL VARIABLES #
+E = 250e6 * pc.eV
+A = 40
+Z = 20
 
 
 def rho_Ch(r, X): 
@@ -8,27 +17,43 @@ def rho_Ch(r, X):
     return rho0 / (1 + np.exp((r-a)/b))
 
 def formFactor(q2, X, Z):
+    (rho0, a, b) = X
+    #assert (a < 7*pc.fm) #since we assume that the nucleus is only a few fm
     q = np.sqrt(q2)
 
     # Sample r
-    N = 100
-    r = np.linspace(0, 8, N) * 1e-15
+    N = 1000
+    r = np.linspace(0, 8, N) * pc.fm
 
     # Calculate Fourier integral
     result = np.zeros_like(q2)
     for i,q_val in enumerate(q):
         integrand = r * rho_Ch(r, X) * np.sin(q_val * r / pc.hbar)
-        integral = np.trapz(integrand, r)
-        fact = 4 * np.pi / (Z * pc.qe * q_val)
+        integral = np.trapz(integrand, x=r)
+        fact = 4 * np.pi * pc.hbar/ (Z * pc.e * q_val)
         result[i] = fact * integral
 
     return result
 
-# Plot sample charge density
-rho0 = 0.07
-a    = 4e-15
-b    = 0.5e-15
-X = (rho0, a, b)
+def theo_cs(Z, E, X, theta):
+    p2 = (E/pc.c)**2 - (pc.me*pc.c)**2
+    q2 = 4 * p2 * np.sin(theta/2)**2
+    F2 = formFactor(q2, X, Z)**2
+
+    return F2 * cs_mott(Z, E, theta)
+
+def proton_count(X):
+    #assert (a < 7*pc.fm) #since we assume that the nucleus is only a few fm
+    N = 1000
+    r = np.linspace(0, 8, N) * pc.fm
+
+    integrand = r**2 * rho_Ch(r, X)
+    integral = np.trapz(integrand, x=r)
+    fact = 4 * np.pi / pc.e
+
+    return fact * integral 
+
+
 
 #r = np.linspace(0, 8) * 1e-15
 #plt.plot(r*1e15, rho_Ch(r,X))
@@ -37,19 +62,50 @@ X = (rho0, a, b)
 #plt.xlabel("radius (fm)")
 #plt.show()
 
+rho0 = 0.07 * pc.e * (pc.fm)**(-3)
+a    = 4e-15
+b    = 0.5e-15
+X = (rho0, a, b)
+
 deg = (2 * np.pi / 360) # One degree in radians
-theta = np.linspace(30, 130) * deg
+theta = np.linspace(30, 130, 500) * deg
 
-Z = 40
-E = 250e6 * pc.eV
-p2 = (E/pc.c)**2 - (pc.me*pc.c)**2
-q2 = 4 * p2 * np.sin(theta/2)**2
 
-F2 = formFactor(q2, X, Z)**2
-
+# Plot sample charge density
 fig = plt.figure()
-plt.plot(theta / deg, F2 * cs_mott(Z, E, theta) / (1e-3 * pc.barn))
+plt.plot(theta / deg, theo_cs(Z, E, X, theta) / (1e-3 * pc.barn))
 plt.yscale('log')
 plt.ylabel("Mott scattering crossection (mb/sr)")
 plt.xlabel("angle (deg)")
 plt.show()
+
+
+
+
+
+
+# NUMERICAL OPTIMIZATION #
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+CSV_fileName = "experimentalData.csv"
+CSV_data     = pd.read_csv(current_path + "\\" + CSV_fileName, sep=',')
+
+header    = CSV_data.columns.values
+exp_theta = CSV_data[header[0]] * (np.pi / 180)
+exp_cs    = CSV_data[header[1]] * (1e-3 * pc.barn)
+exp_error = CSV_data[header[2]] * (1e-3 * pc.barn)
+
+def chi2(X):
+    epsilon = 1e-6
+
+    err = np.sum( ( (theo_cs(Z, E, X, exp_theta) - exp_cs) / exp_error )**2 )
+    constraint = ( (Z - proton_count(X))/epsilon )**2  # correct number of protons in nucleus
+    chi2 = err + constraint
+
+    return chi2
+
+
+# X_star = argmin( chi2(X)) 
+
+X_star = minimize(chi2, X)
+print(X_star)
--- sizeofnucleus/physicsconstants.py
+++ sizeofnucleus/physicsconstants.py
@@ -2,16 +2,18 @@
 eV     = 1.6022e-19         # One electron volt in Joules
 eV_c2  = 1.7827e-36         # eV/c2 in kg
 barn   = 1e-28              # barn
+fm     = 1e-15              # fermi meter
+
 
 # Physical constants
 c      = 2.99792e8          # Speed of light in vacuum
 h      = 6.62607e-34        # Plancks constant
 hbar   = 1.05457e-34        # Plancks reduced constant
-qe     = 1.60218e-19        # Electron charge
+e      = 1.60218e-19        # Electron charge
 NA     = 6.02214e23         # Avogadros number
 kB     = 1.38064e-23        # Bolzmanns constant
 me     = 9.10938e-31        # Mass of electron
-mn     = 1.67493e-17        # Mass of neutron
+mn     = 1.67493e-27        # Mass of neutron
 mp     = 1.67262e-27        # Mass of proton
 MW     = 1.43292e-25        # Mass of W boson
 MZ     = 1.62560e-25        # Mass of Z boson
